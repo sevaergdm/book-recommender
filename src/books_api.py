079308becb68f4f8a440bd730692af297f13fb87
Michael Greaves <mdgmash@gmail.com>
2025-08-15 22:07:17 +0200

    Add open library enrichment

--- src/books_api.py
+++ src/books_api.py
@@ -3,39 +3,101 @@ import argparse
 import requests
 
 
-def search_books_title_author(title, author):
+def google_search_title_author(title, author, max_books_to_fetch=200):
+    all_books = []
     url = r"https://www.googleapis.com/books/v1/volumes"
+    batch_size = 40
     query = f'intitle:"{title}"+inauthor:"{author}"'
-    params = {"q": query, "maxResults": 10}
 
-    try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        data = response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return
+    for i in range(0, max_books_to_fetch, batch_size):
+        params = {
+            "q": query,
+            "maxResults": batch_size,
+            "startIndex": i,
+        }
 
-    if data.get("totalItems", 0) == 0:
-        print(f"No books were found with the title"
-              f" '{title}' and author '{author}'")
-        return
+        try:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            data = response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+            break
 
-    books = data.get("items", [])
+        if not data.get("items"):
+            break
+
+        all_books.extend(data.get("items", []))
+
     search_author = author.lower().strip()
+    search_title = title.lower().strip()
 
     filtered_books = [
-        book for book in books
-        if any(search_author in book_author.lower()
+       book
+       for book in all_books
+       if (
+           any(
+               search_author in book_author.lower()
                for book_author in book.get("volumeInfo", {}).get("authors", [])
-               )
-    ]
+           )
+           and search_title in book.get("volumeInfo", {}).get("title", "").lower()
+       )
+   ]
 
     if not filtered_books:
-        print(f"No books were found with the title"
-              f" '{title}' and author '{author}'")
+        print(f"No books were found with the title" f" '{title}' and author '{author}'")
 
     return filtered_books
+
+
+def enrich_book_data_openlibrary(books):
+    for book in books:
+        isbn13 = next(
+            (
+                item.get("identifier", "")
+                for item in book.get("volumeInfo", {}).get("industryIdentifiers", [])
+                if isinstance(item, dict) and item.get("type", "") == "ISBN_13"
+            ),
+            None
+        )
+
+        if isbn13:
+            try:
+                url = f"http://openlibrary.org/api/volumes/brief/isbn/{isbn13}.json"
+                response = requests.get(url)
+                response.raise_for_status
+                data = response.json()
+
+                if "records" in data and data["records"]:
+                    records_dict = data["records"]
+                    records_data = list(records_dict.values())[0]
+                    details = records_data.get("details", {}).get("details", {})
+                    subjects = details.get("subjects", [])
+
+                    book["openLibrarySubjects"] = subjects
+
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to fetch data from OpenLibrary for ISBN {isbn13}: {e}")
+    return books
+
+
+
+
+def get_book_categories(books):
+    categories = set()
+    for book in books:
+        volume_info = book.get("volumeInfo", {})
+        book_main_category = volume_info.get("mainCategory", "")
+        book_categories = volume_info.get("categories", [])
+        ol_categories = book.get("openLibrarySubjects", [])
+        if book_main_category != "":
+            book_categories.append(book_main_category)
+        if ol_categories:
+            book_categories.extend(ol_categories)
+        if book_categories:
+            for category in book_categories:
+                categories.add(category.lower().strip())
+    return list(categories)
 
 
 def print_book_info(books):
@@ -47,11 +109,15 @@ def print_book_info(books):
         published_date = volume_info.get("publishedDate", "N/A")
         main_category = volume_info.get("mainCategory", "N/A")
         categories = ", ".join(volume_info.get("categories", ["N/A"]))
+        ol_subjects = ", ".join(book.get("openLibrarySubjects", ["N/A"]))
         industry_identifiers = volume_info.get("industryIdentifiers", [])
         isbn13 = next(
-            (item.get("identifier") for item in industry_identifiers
-             if isinstance(item, dict) and item["type"] == "ISBN_13"),
-            "N/A"
+            (
+                item.get("identifier")
+                for item in industry_identifiers
+                if isinstance(item, dict) and item["type"] == "ISBN_13"
+            ),
+            "N/A",
         )
 
         print("-" * 20)
@@ -61,6 +127,7 @@ def print_book_info(books):
         print(f"Published Date: {published_date}")
         print(f"Main Cateogry: {main_category}")
         print(f"Categories: {categories}")
+        print(f"Open Library Subjects: {ol_subjects}")
         print(f"ISBN-13: {isbn13}")
 
 
@@ -73,5 +140,8 @@ if __name__ == "__main__":
     parser.add_argument("--author", required=True, type=str)
     args = parser.parse_args()
 
-    books = search_books_title_author(args.title, args.author)
+    books = google_search_title_author(args.title, args.author)
+    books = enrich_book_data_openlibrary(books)
     print_book_info(books)
+    print("="*10 + "Book Categories" + "="*10)
+    print(get_book_categories(books))
